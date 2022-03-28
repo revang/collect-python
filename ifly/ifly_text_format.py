e02f4a2c6a97d65af100dc0af25996650fd06814
revang <revang.alternative@foxmail.com>
2022-03-28 13:14:35 +0800

    add ifly scripts

--- ifly/ifly_text_format.py
+++ ifly/ifly_text_format.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import re
 
 
 def ifly_text_format(filepath):
@@ -10,13 +9,19 @@ def ifly_text_format(filepath):
     讯飞听见导出文本格式化
     """
     with open(filepath, mode="r", encoding="utf-8") as f:
-        text = f.read()
-        items = re.findall("说话人[^(说话人)]*", text, flags=re.M)
-        for item in items:
-            block1, block2 = item.split("\n", maxsplit=1)  # 字符串分割成两部分
-            speaker, begin_time = block1.split(" ")
-            content = block2.replace("\n", "")
-            print("{} {}: {}".format(begin_time, speaker, content))
+        lines = f.readlines()
+        idx, size = 1, len(lines)
+        while idx < size:
+            if lines[idx].find("说话人") >= 0:
+                speaker, begin_time = lines[idx].split(" ")
+                context = ""
+                idx += 1
+                while idx < size and lines[idx].find("说话人") < 0:
+                    context += lines[idx]
+                    idx += 1
+                begin_time, speaker,  context = begin_time.strip(), speaker.strip(), context.replace("\n", "").strip()
+                print("{} {}: {}".format(begin_time, speaker, context))
+            idx += 1
 
 
 def main():
