38eb655d1a451919284eda8ee1d8be60786785c7
revang <revang.alternative@foxmail.com>
2023-01-06 02:44:27 +0800

    添加sqlexecutetool

---
+++ SQLExecuteTool/README.txt
@@ -0,0 +1,2 @@
+# 打包
+pyinstaller -F main.py -i resources/hundsun.ico -n SQLExecuteTool -w
--- SQLExecuteTool/main.py
+++ SQLExecuteTool/main.py
@@ -1,5 +1,6 @@
 import os
 
+
 def get_sql_file_list(target_path):
     """
     获取目标文件夹下的sql文件
@@ -10,16 +11,18 @@ def get_sql_file_list(target_path):
         - fnc ---> function  函数
         - trg ---> trigger   触发器
     """
-    sql_file_list=[]
+    sql_file_list = []
     for root, dirs, files in os.walk(target_path):
         for filename in files:
-            if filename.endswith(('.sql','.pck','.prc','.fnc','.trg')) and filename[0:10]!="SQLExecute":
+            if filename.endswith(('.sql', '.pck', '.prc', '.fnc', '.trg')) and filename[0:10] != "SQLExecute":
                 sql_file_list.append(os.path.join(root, filename))
     return sql_file_list
 
+
 if __name__ == "__main__":
-    file_list=get_sql_file_list(os.getcwd())
-    execute_file="SQLExecute.sql"
-    with open(execute_file,"w") as f:
+    file_list = get_sql_file_list(os.getcwd())
+    execute_file = "SQLExecute.sql"
+    with open(execute_file, "w") as f:
         for filepath in file_list:
-            f.write("@{}\n".format(filepath))
+            f.write("prompt executing '{}'\n".format(filepath))
+            f.write("                @'{}'\n".format(filepath))
