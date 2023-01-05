23794ab4c9597da49402b4bc68d33d72e69d8688
revang <revang.alternative@foxmail.com>
2023-01-06 04:13:41 +0800

    修改注释

--- spider/wenku8/wenku8.py
+++ spider/wenku8/wenku8.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 cookies = {}
-params = {"action": "login", "username": "dujiaoshou", "password": "dujiaoshou801"}  # form data
+params = {"action": "login", "username": "dujiaoshou", "password": "dujiaoshou801"}  # plyload / form data
 
 
 def login():
